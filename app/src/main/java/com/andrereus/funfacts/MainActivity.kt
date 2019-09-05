000623961a698d40387ae4db04499a2470fd27c8
Andre Reus <andrereus@users.noreply.github.com>
2019-09-05 11:16:23 +0200

    Add functionality

--- app/src/main/java/com/andrereus/funfacts/MainActivity.kt
+++ app/src/main/java/com/andrereus/funfacts/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.andrereus.funfacts
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val factBook = FactBook()
+    private val colorWheel = ColorWheel()
+
+    private var factTextView: TextView? = null
+    private var showFactButton: Button? = null
+    private var relativeLayout: RelativeLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        factTextView = findViewById(R.id.factTextView)
+        showFactButton = findViewById(R.id.showFactButton)
+        relativeLayout = findViewById(R.id.relativeLayout)
+
+        showFactButton!!.setOnClickListener {
+            val fact = factBook.getFact()
+            val color = colorWheel.getColor()
+
+            factTextView!!.text = fact
+            relativeLayout!!.setBackgroundColor(color)
+            showFactButton!!.setTextColor(color)
+        }
     }
 }
